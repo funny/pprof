7a6aff76d6a204184d5bab95516cf38886abd7b5
idada <bg5sbk@gmail.com>
2014-11-21 11:20:04 +0800

    improve GC summary output.

--- gc_summary.go
+++ gc_summary.go
@@ -1,6 +1,7 @@
 package overall
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"runtime"
@@ -10,7 +11,21 @@ import (
 
 var startTime = time.Now()
 
-func GCSummary(writer io.Writer) {
+type GCSummaryInfo struct {
+	NumGC      int64
+	LastPause  time.Duration
+	PauseAvg   time.Duration
+	Overhead   float64
+	Alloc      uint64
+	Sys        uint64
+	AllocRate  uint64
+	Histogram1 time.Duration
+	Histogram2 time.Duration
+	Histogram3 time.Duration
+}
+
+// Get GC summary.
+func GCSummary() GCSummaryInfo {
 	gcstats := debug.GCStats{PauseQuantiles: make([]time.Duration, 100)}
 	debug.ReadGCStats(&gcstats)
 
@@ -18,32 +33,75 @@ func GCSummary(writer io.Writer) {
 	runtime.ReadMemStats(&memStats)
 
 	elapsed := time.Now().Sub(startTime)
-	allocatedRate := float64(memStats.TotalAlloc) / elapsed.Seconds()
+
+	summary := GCSummaryInfo{
+		Alloc:     memStats.Alloc,
+		Sys:       memStats.Sys,
+		AllocRate: uint64(float64(memStats.TotalAlloc) / elapsed.Seconds()),
+	}
 
 	if gcstats.NumGC > 0 {
-		overhead := float64(gcstats.PauseTotal) / float64(elapsed) * 100
-
-		fmt.Fprintf(writer,
-			"NumGC: %d, LastPause: %v, Pause(Avg): %v, Overhead: %3.2f%%, Alloc: %s, Sys: %s, Alloc(Rate): %s/s, Histogram: %v %v %v\n",
-			gcstats.NumGC,
-			gcstats.Pause[0],
-			durationAvg(gcstats.Pause),
-			overhead,
-			readableSize(memStats.Alloc),
-			readableSize(memStats.Sys),
-			readableSize(uint64(allocatedRate)),
-			gcstats.PauseQuantiles[94],
-			gcstats.PauseQuantiles[98],
-			gcstats.PauseQuantiles[99],
-		)
-	} else {
-		fmt.Fprintf(writer,
-			"NumGC: 0, Alloc: %s, Sys:%s, Alloc(Rate): %s/s\n",
-			readableSize(memStats.Alloc),
-			readableSize(memStats.Sys),
-			readableSize(uint64(allocatedRate)),
-		)
+		summary.NumGC = gcstats.NumGC
+		summary.LastPause = gcstats.Pause[0]
+		summary.PauseAvg = durationAvg(gcstats.Pause)
+		summary.Overhead = float64(gcstats.PauseTotal) / float64(elapsed) * 100
+		summary.Histogram1 = gcstats.PauseQuantiles[94]
+		summary.Histogram2 = gcstats.PauseQuantiles[98]
+		summary.Histogram3 = gcstats.PauseQuantiles[99]
 	}
+
+	return summary
+}
+
+// Humman readable string.
+func (summary *GCSummaryInfo) String() string {
+	buffer := new(bytes.Buffer)
+	summary.Write(buffer)
+	return buffer.String()
+}
+
+// CSV string.
+func (summary *GCSummaryInfo) CSV() string {
+	buffer := new(bytes.Buffer)
+	summary.WriteCSV(buffer)
+	return buffer.String()
+}
+
+// Write as humman readable format.
+func (summary *GCSummaryInfo) Write(writer io.Writer) {
+	fmt.Fprintf(writer,
+		"NumGC: %d, LastPause: %v, Pause(Avg): %v, Overhead: %3.2f%%, Alloc: %s, Sys: %s, Alloc(Rate): %s/s, Histogram: %v %v %v",
+		summary.NumGC,
+		summary.LastPause,
+		summary.PauseAvg,
+		summary.Overhead,
+		formatSize(summary.Alloc),
+		formatSize(summary.Sys),
+		formatSize(summary.AllocRate),
+		summary.Histogram1,
+		summary.Histogram2,
+		summary.Histogram3,
+	)
+}
+
+// GC summary CSV column names.
+const GCSummaryColumns = "NumGC,LastPause,Pause(Avg),Overhead,Alloc,Sys,Alloc(Rate),Histogram1,Histogram2,Histogram3"
+
+// Write as CSV format.
+func (summary *GCSummaryInfo) WriteCSV(writer io.Writer) {
+	fmt.Fprintf(writer,
+		"%d,%d,%d,%3.2f,%d,%d,%d,%d,%d,%d",
+		summary.NumGC,
+		summary.LastPause,
+		summary.PauseAvg,
+		summary.Overhead,
+		summary.Alloc,
+		summary.Sys,
+		summary.AllocRate,
+		summary.Histogram1,
+		summary.Histogram2,
+		summary.Histogram3,
+	)
 }
 
 func durationAvg(items []time.Duration) time.Duration {
@@ -54,15 +112,15 @@ func durationAvg(items []time.Duration) time.Duration {
 	return time.Duration(int64(sum) / int64(len(items)))
 }
 
-func readableSize(bytes uint64) string {
+func formatSize(bytes uint64) string {
 	switch {
 	case bytes < 1024:
-		return fmt.Sprintf("%d B", bytes)
+		return fmt.Sprintf("%dB", bytes)
 	case bytes < 1024*1024:
-		return fmt.Sprintf("%.2f K", float64(bytes)/1024)
+		return fmt.Sprintf("%.2fK", float64(bytes)/1024)
 	case bytes < 1024*1024*1024:
-		return fmt.Sprintf("%.2f M", float64(bytes)/1024/1024)
+		return fmt.Sprintf("%.2fM", float64(bytes)/1024/1024)
 	default:
-		return fmt.Sprintf("%.2f G", float64(bytes)/1024/1024/1024)
+		return fmt.Sprintf("%.2fG", float64(bytes)/1024/1024/1024)
 	}
 }
---
+++ gc_summary_test.go
@@ -0,0 +1,19 @@
+package overall
+
+import (
+	"runtime"
+	"testing"
+)
+
+func Test_GCSummary(t *testing.T) {
+	summary := GCSummary()
+	println(" ", summary.String())
+
+	runtime.GC()
+
+	summary = GCSummary()
+	println(" ", summary.String())
+
+	println(" ", GCSummaryColumns)
+	println(" ", summary.CSV())
+}
